18ec67f08a6f8e11f632072cd5ff2cf40967e23a
astitva98 <astitvasood98@gmail.com>
2019-11-15 17:58:40 +0530

    changs

--- final2 copy 2.py
+++ final2 copy 2.py
@@ -12,8 +12,11 @@ mixer.music.load("beep-02.mp3")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(22,GPIO.OUT)
+GPIO.setup(5, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(6, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(17, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(13, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(12, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 camera=PiCamera()
 
@@ -22,10 +25,18 @@ Trig = 18
 Echo = 24
 
 def playMusic():
-    mixer.music.play("beep-02.mp3")
-    flag=0
+    mixer.music.load("beep-02.mp3")
+    mixer.music.play(2)
+    flag = 0
 
-
+def camer():
+    GPIO.output(22, 1)
+    camera.start_preview()
+    sleep(3)
+    camera.capture('/home/pi/Desktop/Visionary-Aid/sample1.jpg')
+    GPIO.output(22, 0)
+    camera.stop_preview()
+    subprocess.call("./automate.sh")
 
 def close(signal, frame):
     print("\nTurning off the system...\n")
@@ -37,6 +48,7 @@ signal.signal(signal.SIGINT, close)
 GPIO.setup(Trig, GPIO.OUT)
 GPIO.setup(Echo, GPIO.IN)
 i=1
+stpa = 0
 while True:
     input_state=GPIO.input(12)
     input_state2=GPIO.input(23)
@@ -55,41 +67,41 @@ while True:
     flagg1 = 1
 
     if input_state2==False:
-        GPIO.output(22, 1)
-        camera.start_preview()
-        sleep(3)
-        camera.capture('/home/pi/Desktop/panel1/sample.jpg')
-        GPIO.output(22, 0)
-        camera.stop_preview()
-        subprocess.call("./automate.sh")
-        mixer.music.load("output.wav")
-        time=mixer.music.get_length("output.wav")
-        mixer.music.play("output.wav")
-        offset = 0.0
-        flagg1=0
-        if input_state3==false:
-            mixer.music.pause()
-
-        if input_state3==false:
-            mixer.music.unpause()
-
-        if imput_state4 == false:
-            
-            mixer.music.pause()
-            mixer.music.set_pos(5)
-            mixer.music.unpause()
-
-        if imput_state5 == false:
-            mixer.music.pause()
-            mixer.music.set_pos(5)
-            mixer.music.unpause()
+        mixer.music.load("ooo.mp3")
+        mixer.music.play(0)
+        while(mixer.music.get_busy()):
+            input_state=GPIO.input(12)
+            input_state2=GPIO.input(23)
+            input_state3=GPIO.input(13)
+            input_state4=GPIO.input(5)
+            input_state5=GPIO.input(6)
+            if input_state3==False: 
+                if stpa == 0:
+                    mixer.music.pause()
+                    print("Pause")
+                    stpa = 1
+                else:
+                    mixer.music.unpause()
+                    print("Play")
+                    stpa = 0
+                time.sleep(0.1)
+            if input_state4 == False:
+                print("Forward")
+                mixer.music.set_pos(10)
+                time.sleep(0.1)
+            if input_state5 == False:
+                print("Rewind")
+                mixer.music.set_pos(-15)
+                time.sleep(0.1)
+        
+        mixer.music.load("beep-02.mp3")
 
         
     if mixer.music.get_busy() == False:
-    	flagg1=1
+        flagg1=1
 
-    if flagg1 == 0:	
-    	continue
+    if flagg1 == 0: 
+        continue
 
 
 
